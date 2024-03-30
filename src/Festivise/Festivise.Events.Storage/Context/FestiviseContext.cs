d2b296ec495f1c989feb7c4e48689ccfb3412f75
KenGod88 <ken.godfroid@student.hogent.be>
2024-03-30 17:35:58 +0100

    Added datalayer with enity framework - made context for DB

---
+++ src/Festivise/Festivise.Events.Storage/Context/FestiviseContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Festivise.Events.Domain.Models;
+
+namespace Festivise.Events.Storage.Context
+{
+    internal class FestiviseContext : DbContext
+    {
+        public DbSet<Domain.Models.Event> Events { get; set; }
+        public DbSet<Domain.Models.Act> Acts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=laptop_ken\\sqlexpress;Initial Catalog=Festivise;Integrated Security=True;Pooling=False;Encrypt=False;Trust Server Certificate=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.Models.Event>().HasKey(e => e.Id);
+            modelBuilder.Entity<Domain.Models.Act>().HasKey(a => a.Id);
+        }
+    }
+}
