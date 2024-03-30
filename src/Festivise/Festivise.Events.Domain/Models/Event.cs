421a10e95b705b68193dfc434858cccfb95cd6d7
KenGod88 <ken.godfroid@student.hogent.be>
2024-03-30 18:19:21 +0100

    Added use of Enum

--- src/Festivise/Festivise.Events.Domain/Models/Event.cs
+++ src/Festivise/Festivise.Events.Domain/Models/Event.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Festivise.Events.Api.Contracts.Enums;
 
 namespace Festivise.Events.Domain.Models
 {
@@ -24,7 +20,7 @@ namespace Festivise.Events.Domain.Models
         public DateTime EndTime { get; set; }
 
 
-        public string Venue { get; set; }
+        public VenueEnum Venue { get; set; }
 
 
         public List<Act>? Acts { get; set; }
