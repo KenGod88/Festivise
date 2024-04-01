988ce948f371937402faf7158cb389432f253086
KenGod88 <ken.godfroid@student.hogent.be>
2024-04-01 17:49:48 +0200

    Added EnumOptions - Options patern -  services

--- src/Festivise/Festivise.Events.Api/Program.cs
+++ src/Festivise/Festivise.Events.Api/Program.cs
@@ -1,15 +1,40 @@
+using Festivise.Events.Api.Services;
+using Festivise.Events.Storage;
+using Festivise.Events.Storage.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 namespace Festivise.Events.Api
+
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+
+           
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllers();
+
+            builder.Services.AddControllers().AddJsonOptions(opt =>
+            {
+                opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            }).AddMvcOptions(options =>
+            {
+                options.SuppressAsyncSuffixInActionNames = false;
+            });
+
+            builder.Services.AddScoped<IEventRepository, EventRepository>();
+            builder.Services.AddScoped<IEventService, EventService>();
+
+
+            builder.Services.AddDbContext<FestiviseContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetSection("EventRepositoryOptions:ConnectionString").Value));
+
+
 
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapControllers();
 
             app.Run();
         }
