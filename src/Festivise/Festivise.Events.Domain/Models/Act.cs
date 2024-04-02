9b96701d6fd55d72bd221dd58c53daffc83f83ab
KenGod88 <ken.godfroid@student.hogent.be>
2024-04-02 16:45:42 +0200

    Updated Act for use in Schedule

--- src/Festivise/Festivise.Events.Domain/Models/Act.cs
+++ src/Festivise/Festivise.Events.Domain/Models/Act.cs
@@ -4,6 +4,8 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
         public TimeSpan Duration { get; set; }
 
         public Guid EventId { get; set; }
