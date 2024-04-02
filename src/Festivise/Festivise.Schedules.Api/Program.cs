99ccfee59cc5bbf9042697d6062a0153b0ec2010
KenGod88 <ken.godfroid@student.hogent.be>
2024-04-02 16:46:20 +0200

    Added QuestPDF to project and first design of PDF

--- src/Festivise/Festivise.Schedules.Api/Program.cs
+++ src/Festivise/Festivise.Schedules.Api/Program.cs
@@ -1,13 +1,19 @@
+using QuestPDF.Infrastructure;
+
 namespace Festivise.Schedules.Api
+
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            QuestPDF.Settings.License = LicenseType.Community;
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            
 
             app.Run();
         }
